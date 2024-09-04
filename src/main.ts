472811985c4ab1422a7bb9da8bb5ca8a14e2cd1d
stormymayday <stormymayday7@gmail.com>
2024-09-04 13:42:14 -0700

    Selecting form, input, and list elements

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <main>
-      <h2>Tasks</h2>
-      <form class="form">
-        <input type="text" class="form-input" />
-        <button type="submit" class="btn">add task</button>
-      </form>
-      <ul class="list"></ul>
-      <button class="test-btn">click me</button>
-    </main>
+    <h2>Tasks</h2>
+    <form class="form">
+      <input type="text" class="form-input" />
+      <button type="submit" class="btn">add task</button>
+    </form>
+    <ul class="list"></ul>
+  </main>
 `;
+
+// Selecting Elements:
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+const taskList = document.querySelector<HTMLUListElement>(".list");
+console.log(taskList);
